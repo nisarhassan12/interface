730f6739bd22ba128665c9cd4401378ded96e87f
nick shook <nick@shook.family>
2020-06-06 21:03:36 -0700

    loginAsPortalUser cypress test (#141)
    * loginAsPortalUser cypress test
    
    This test uses code from [this
    project](https://github.com/dunson062786/login-programmatically-into-auth0-with-cypress/blob/master/cypress/integration/login_spec.js)
    to use the Auth0 management API to log in via a post request and create
    an access_token that we can use to test user sessions.
    
    Co-authored-by: Nicholas Shook <nick@neonlaw.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,6 @@
+/// <reference types="cypress" />
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/camelcase */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -25,3 +28,56 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add(
+  'loginAsPortalUser',
+  () => {
+    cy.log('Logging in as portal@neonlaw.com');
+    const options = {
+      body: {
+        'audience': Cypress.env('AUDIENCE_URL'),
+        'client_id': Cypress.env('AUTH_CLIENT_ID'),
+        'client_secret': Cypress.env('AUTH_CLIENT_SECRET'),
+        'grant_type': 'http://auth0.com/oauth/grant-type/password-realm',
+        'password': Cypress.env('PORTAL_USER_PASSWORD'),
+        realm: 'Username-Password-Authentication',
+        'scope': 'openid profile email',
+        'username': 'portal@neonlaw.com',
+      },
+      method: 'POST',
+      url: Cypress.env('AUTH_URL'),
+    };
+    cy.request(options).then(({ body }) => {
+      const { access_token, expires_in, id_token } = body;
+
+      cy.server();
+
+      // intercept Auth0 request for token and return what we have
+      cy.route({
+        method: 'POST',
+        response: {
+          access_token: access_token,
+          expires_in: expires_in,
+          id_token: id_token,
+          scope: 'openid profile email',
+          token_type: 'Bearer',
+        },
+        url: 'oauth/token',
+      });
+      // Auth0 SPA SDK will check for value in cookie to get appState
+      // add validate nonce (which has been removed for simplicity)
+      const stateId = 'test';
+      cy.setCookie(
+        `a0.spajs.txs.${stateId}`,
+        encodeURIComponent(JSON.stringify({
+          'appState': { targetUrl: '/' },
+          'audience': 'default',
+          'redirect_uri': 'http://127.0.0.1:8000',
+          'scope': 'openid profile email',
+        }))
+      ).then(() => {
+        cy.visit(`/?code=test&state=${stateId}`);
+      });
+    });
+  }
+);
