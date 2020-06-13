6f1b6916282eb1137ede3654501ad6805363ba9c
Nicholas Shook <nick@neonlaw.com>
2020-06-12 18:51:30 -0700

    autosave

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -2,10 +2,15 @@
 /* eslint-disable no-undef */
 
 describe('Blog page', () => {
-  beforeEach(() => {
+  it('has an edit button that loads the edit page on GitHub', () => {
     cy.visit('/blog');
+    cy.get('[data-cy="edit-on-github"]').should('be.visible');
   });
-  it('has an edit button that loads GitHub', () => {
+});
+
+describe('Setting Up Shop', () => {
+  it('has an edit button that loads the edit page on GitHub', () => {
+    cy.visit('/blog/setting-up-shop');
     cy.get('[data-cy="edit-on-github"]').should('be.visible');
   });
 });
--- src/components/navigation/publicLinks.ts
+++ src/components/navigation/publicLinks.ts
@@ -3,26 +3,26 @@ import { useIntl } from 'gatsby-plugin-intl';
 
 export const PublicLinks = () => {
   const intl = useIntl();
-  // const label=intl.formatMessage({ id: 'footer.privacy_Policy' });
-  const links1 = [
-    { label:intl.formatMessage({ id: 'navigation_publicL.upward' }) ,
-      route: '/upward-mobility' },
-    { label: intl.formatMessage({ id: 'navigation_publicL.practiceAreas' }),
-      route: '/practice-areas' },
-    { label: intl.formatMessage({ id: 'navigation_publicL.blog' }),
-      route: '/blog' },
-    { label: intl.formatMessage({ id: 'navigation_publicL.contact' }),
-      route: '/contact' }
+  const links = [
+    {
+      label: intl.formatMessage({ id: 'navigation_publicL.upward' }),
+      route: '/upward-mobility'
+    },
+    {
+      label: intl.formatMessage({ id: 'navigation_publicL.practiceAreas' }),
+      route: '/practice-areas'
+    },
+    {
+      label: intl.formatMessage({ id: 'navigation_publicL.blog' }),
+      route: '/blog'
+    },
+    {
+      label: intl.formatMessage({ id: 'navigation_publicL.contact' }),
+      route: '/contact'
+    }
   ];
 
   return (
-    links1
+    links
   );
 };
-
-// export const publicLinks = [
-//   { label: 'Upward Mobility', route: '/upward-mobility' },
-//   { label: 'Practice Areas', route: '/practice-areas' },
-//   { label: 'Blog', route: '/blog' },
-//   { label: 'Contact', route: '/contact' }
-// ];
