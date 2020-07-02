e4b5a42a3e979b19079a5e18bc77189bfef8dba0
Nicholas Shook <nick@neonlaw.com>
2020-07-01 22:20:17 -0700

    right url for rickie

--- cypress/integration/justiceForRickieSlaughter.spec.ts
+++ cypress/integration/justiceForRickieSlaughter.spec.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 
-describe('Visiting /justice-for-rickie-slaughter', function () {
+describe('Visiting /pro-bono/justice-for-rickie-slaughter', function () {
   it('renders a link to the contact form', function () {
-    cy.visit('/justice-for-rickie-slaughter');
+    cy.visit('/pro-bono/justice-for-rickie-slaughter');
     cy.get('[data-cy="edit-on-github"]').should('be.visible');
   });
 });
