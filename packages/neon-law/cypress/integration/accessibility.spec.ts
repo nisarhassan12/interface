c0b89e1e5330dc96b8b9267a96c77b69ae794b9d
nick shook <nick@shook.family>
2020-07-24 22:49:16 -0700

    remove I accept

--- packages/neon-law/cypress/integration/accessibility.spec.ts
+++ packages/neon-law/cypress/integration/accessibility.spec.ts
@@ -7,7 +7,7 @@ function terminalLog(violations) {
   cy.task(
     'log',
     `${violations.length} accessibility violation${
-      violations.length === 1 ? '' : 's'
+    violations.length === 1 ? '' : 's'
     } ${violations.length === 1 ? 'was' : 'were'} detected`
   );
   // pluck specific keys to keep the table readable
@@ -29,7 +29,6 @@ describe('Accessibility tests', () => {
     cy.injectAxe();
   });
   it('Has no detectable accessibility violations on load', () => {
-    cy.contains('I Accept').click();
     cy.checkA11y(null, {
       rules: {
         'color-contrast': { enabled: false },
