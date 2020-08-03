65803d7688be7d3d6fd95f8ae38a2a5790e0dd2a
nick shook <nick@shook.family>
2020-08-02 20:58:15 -0700

    moved shared folder to shared-ui (#588)
    * moved shared folder to shared-ui
    
    This makes the package folder name in sync with the package name
    
    * update package and comment out code

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     'build/',
     'node_modules/',
     'public/',
-    'packages/shared/src/utils/api.tsx'
+    'packages/shared-ui/src/utils/api.tsx'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
