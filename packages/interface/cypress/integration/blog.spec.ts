28e6cd1fb009bcd17329ef5f74dd4a8e51f297ba
nick shook <nick@shook.family>
2020-08-11 18:00:37 -0700

    Added Slaughter v. Escamilla order (#676)
    * Added Slaughter v. Escamilla order
    
    * added basic blog spec

--- packages/interface/cypress/integration/blog.spec.ts
+++ packages/interface/cypress/integration/blog.spec.ts
@@ -4,5 +4,7 @@ describe('CIDR blocks blog page', () => {
   it('has an edit button that loads the edit page on GitHub', () => {
     cy.visit('/blog/cidr-blocks');
     cy.get('[data-cy="edit-on-github"]').should('be.visible');
+    cy.visit('/blog/slaughter-v-escamilla');
+    cy.get('[data-cy="edit-on-github"]').should('be.visible');
   });
 });
