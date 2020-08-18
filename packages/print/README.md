cedbff1e942f0bf54809dfd642692317a1ac9edc
nick shook <nick@shook.family>
2020-08-17 21:50:14 -0700

    Move saving PDF into separate useEffect function (#712)
    ```
    yarn run-script ./packages/print/src/index.tsx --file ~/work/matters/2c43fe5f-bfee-4c7c-a7c8-1f82161cee1b/writing/reply_to_motion_to_vacate.mdx
    ```
    
    TODO: Add a MDX Runtime wrapper that maps **every** component to its
    React-PDF counterpart.

--- packages/print/README.md
+++ packages/print/README.md
@@ -7,5 +7,5 @@ A CLI to parse MDX files and generate PDFs
 In the root of this directory, run:
 
 ```
-yarn run-script ./packages/print/src/index.tsx --file=./path/to.md
+yarn run-script ./packages/print/src/index.tsx --file ./path/to.mdx
 ```
--- packages/print/src/index.tsx
+++ packages/print/src/index.tsx
@@ -1,5 +1,6 @@
 import {
   Document,
+  Font,
   Page,
   default as ReactPDF,
   StyleSheet,
@@ -7,19 +8,50 @@ import {
   View
 } from '@react-pdf/renderer';
 import { Text as InkText, render } from 'ink';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { argv } from 'yargs';
+import mdx from '@mdx-js/mdx';
 import { readFileSync } from 'fs';
+
+
+Font.register({
+  family: 'CharisSIL',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  src: 'https://www.neonlaw.com/fonts/charis/CharisSIL-R.ttf'
+});
+Font.register({
+  family: 'CharisSIL',
+  fontStyle: 'italic',
+  fontWeight: 400,
+  src: 'https://www.neonlaw.com/fonts/charis/CharisSIL-I.ttf'
+});
+Font.register({
+  family: 'CharisSIL',
+  fontStyle: 'normal',
+  fontWeight: 700,
+  src: 'https://www.neonlaw.com/fonts/charis/CharisSIL-B.ttf'
+});
+Font.register({
+  family: 'CharisSIL',
+  fontStyle: 'italic',
+  fontWeight: 700,
+  src: 'https://www.neonlaw.com/fonts/charis/CharisSIL-BI.ttf'
+});
 
 const styles = StyleSheet.create({
   page: {
     backgroundColor: '#E4E4E4',
     flexDirection: 'row',
+    fontFamily: 'CharisSIL',
   },
   section: {
     flexGrow: 1,
     margin: 10,
     padding: 10,
+  },
+  text: {
+    fontFamily: 'CharisSIL'
   }
 });
 
@@ -27,52 +59,42 @@ const MyDocument = ({ text }) => (
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>{text}</Text>
+        <Text style={styles.text}>
+          rar
+        </Text>
+        <Text style={styles.text}>
+          {mdx.sync(text)}
+        </Text>
       </View>
     </Page>
   </Document>
 );
 
 const CLI = () => {
-  if (argv.file) {
-    try {
-      const filename = `${argv.file}`;
-      const file = readFileSync(filename, 'utf-8').toString();
+  const [pdfFile, saved] = useState(false);
 
-      ReactPDF.render(<MyDocument text={file} />, `${filename}.pdf`)
-        .then(res => {
-          console.log(res);
-          return (
-            <>
-              <InkText>Writing the PDF file: {`${filename}.pdf`}</InkText>
-            </>
-          );
-        })
-        .catch(err => {
-          console.log(err);
-          return (
-            <>
-              <InkText>Error, you need to pass in a valid --file</InkText>
-            </>
-          );
-        });
-    }
-    catch (err) {
-      console.log(err);
+  useEffect(() => {
+    savePdf();
+  });
 
-      return (
-        <>
-          <InkText>Error, you need to pass in a valid --file</InkText>
-        </>
-      );
-    }
-  }
+  const filename = `${argv.file}`;
+  const file = readFileSync(filename, 'utf-8').toString();
+
+  const savePdf = async () => {
+    if (!argv.file) { return; }
+    await ReactPDF.render(<MyDocument text={file} />, `${filename}.pdf`)
+      .then(() => saved(true))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <InkText>Error, you need to pass in a --file</InkText>
+      <InkText>
+        PDF file at {`${filename}.pdf`} is saved: {pdfFile ? 'true' : 'false'}
+      </InkText>
     </>
   );
+
 };
 
 render(<CLI />);
