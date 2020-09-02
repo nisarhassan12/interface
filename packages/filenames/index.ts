b80f5872527910be30be69f13526db9fb31b4321
nick shook <nick@shook.family>
2020-09-02 16:35:38 -0700

    filename package planning *NO PR* (#759)

---
+++ packages/filenames/index.ts
@@ -0,0 +1,10 @@
+// export { MatterFoldernames } from './src/folders/matters';
+// export { BillsFoldernames } from './src/folders/bills';
+
+// export { BillsPdfs } from './src/pdfs/accounting/bills';
+// export { InvoicePdfs } from './src/pdfs/accounting/invoices';
+
+// export { NDevCourtPdfs } from './src/pdfs/courtFilings';
+// export { EighthDistrictOfNVCourtPdfs } from './src/pdfs/courtFilings';
+
+// export { MDX } from './src/mdx';
